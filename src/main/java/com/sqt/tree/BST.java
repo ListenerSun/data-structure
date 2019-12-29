1d3ca054701f75a72a2ad04898a9dcdf94f4645b
ListenerSun <ListenerSun@163.com>
2019-12-29 23:30:26 +0800

    add 二分搜索树的 删除最大值 和 删除任意元素 和 深度遍历 和 广度遍历 方法

--- src/main/java/com/sqt/tree/BST.java
+++ src/main/java/com/sqt/tree/BST.java
@@ -1,5 +1,8 @@
 package com.sqt.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @Description: 二分搜索树
  * 元素具有可比性
@@ -18,6 +21,10 @@ public class BST<E extends Comparable<E>> {
      */
     Node root;
 
+    public BST() {
+
+    }
+
     public BST(int size) {
         this.size = size;
         this.root = null;
@@ -30,11 +37,25 @@ public class BST<E extends Comparable<E>> {
      * @return
      */
     public void add(E e) {
-        add(root, e);
+        root = add(root, e);
+    }
+
+    /**判断 二分搜索树是否为空
+     * @return
+     */
+    public boolean isEmpty(){
+        return size == 0;
     }
 
     /**
-     * add 递归函数
+     * 获取二分搜索树中元素的个数
+     */
+    public int size(){
+        return size;
+    }
+
+    /**
+     * add 递归函数 返回新增元素后二分搜索书的根
      *
      * @param node
      * @param e
@@ -57,26 +78,180 @@ public class BST<E extends Comparable<E>> {
         return node;
     }
 
-    /** Is Contains an Element
+    /**
+     * Is Contains an Element
+     *
      * @param e
      * @return
      */
-    public boolean contains(E e){
-        return contains(root,e);
+    public boolean contains(E e) {
+        return contains(root, e);
     }
 
     private boolean contains(Node node, E e) {
         //不包含
-        if (null == node){
+        if (null == node) {
             return false;
         }
         //递归调用
-        if (e.compareTo(node.e) == 0){
+        if (e.compareTo(node.e) == 0) {
             return true;
-        }else if (e.compareTo(node.e) < 0){
-            return contains(node.left,e);
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else {
+            return contains(node.right, e);
+        }
+    }
+
+    /**
+     * 深度优先遍历
+     */
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        //前序遍历
+//        System.out.println(node.e);
+        preOrder(node.left);
+        //中序遍历
+//        System.out.println(node.e);
+        preOrder(node.right);
+        //后序遍历
+        System.out.println(node.e);
+    }
+
+    /**
+     * 广度优先遍历
+     */
+    public void levelOrder() {
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node node = q.remove();
+            System.out.println(node.e);
+            if (node.left != null) {
+                q.add(node.left);
+            }
+            if (node.right != null) {
+                q.add(node.right);
+            }
+        }
+
+    }
+
+    /**
+     * 查找最大元素
+     *
+     * @return
+     */
+    public E getMax() {
+        if (root == null) {
+            return null;
+        }
+        return getMax(root).e;
+    }
+
+    /**查找以 node 为根节点二分搜索树中的最大值的节点，并返回最大节点
+     * @param node
+     * @return
+     */
+    private Node getMax(Node node) {
+        if (node.right == null) {
+            return node;
+        }
+        return getMax(node.right);
+    }
+
+    /**
+     * 删除最大的元素，并返回删除的元素值
+     */
+    public E deleteMax() {
+        E max = getMax();
+        root = removeMax(root);
+        return max;
+
+    }
+
+    /**
+     * 删除最大元素，返回删除后新的二分搜索树的根
+     *
+     * @param node
+     * @return
+     */
+    private Node removeMax(Node node) {
+        if (node == null) {
+            return null;
+        }
+        // 此时  node 就是最大的元素，将node左子树的根返回；
+        if (node.right == null) {
+            Node left = node.left;
+            node.left = null;
+            size--;
+            return left;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    /**
+     * 删除任意一个元素
+     * @param e
+     */
+    public void deleteNode(E e){
+        if (root == null){
+            throw new IllegalArgumentException("BST is null!");
+        }
+        root = deleteNode(root,e);
+    }
+
+    /** 删除以 node 为根的二分搜索树中 e 的节点,递归算法
+     *  返回删除元素后新的二分搜索树的根
+     * @param node
+     * @param e
+     * @return
+     */
+    private Node deleteNode(Node node, E e) {
+        if (node == null){
+            System.out.println("No an Element in BST ！");
+            return null;
+        }
+        // e 元素在 node 的右子树中
+        if (e.compareTo(node.e) > 0){
+            node.right = deleteNode(node.right,e);
+            return node;
+        }else if (e.compareTo(node.e) < 0){ // e 元素在 node 的左子树中
+            node.left = deleteNode(node.left,e);
+            return node;
         }else {
-            return contains(node.right,e);
+            //此时 node.e == e,需要将 node 节点移除掉
+            if (node.right == null){
+                Node left = node.left;
+                node.left = null;
+                size--;
+                return left;
+            }else if (node.left == null){
+                Node right = node.right;
+                node.right = null;
+                size--;
+                return right;
+            }else{
+                // node 左右子树都不为空
+                // 找到比待删除节点大的最小值,即待删除节点右子树中的最小值或者待删除节点左树的最大值
+                // 用这个节点代替待删除接节点
+
+                //先定义一个返回值获取左子树中的最大节点
+                Node successCur = getMax(node.left);
+                // 需要将 node 左子树中的最大元素删除掉，然后赋值给successCur
+                successCur.left = removeMax(node.left);
+                successCur.right = node.right;
+                //此时 node 节点已经没用
+                node.left = node.right = null;
+                return successCur;
+            }
         }
     }
 
---
+++ src/main/java/com/sqt/tree/BSTTest.java
@@ -0,0 +1,26 @@
+package com.sqt.tree;
+
+/**
+ * @Description:
+ * @author: ListenerSun(男, 未婚) 微信:810548252
+ * @Date: Created in 2019-12-29 20:57
+ */
+public class BSTTest {
+
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<Integer>();
+        int[] nums = {5,2,6,9,1,3};
+        for (int i : nums){
+            bst.add(i);
+        }
+//        bst.preOrder();
+//        System.out.println("/r/n");
+////        bst.levelOrder();
+//        System.out.println(bst.deleteMax());
+//        System.out.println("/r/n");
+        bst.deleteNode(6);
+        bst.deleteNode(1);
+        bst.preOrder();
+        System.out.println(bst.size);
+    }
+}
